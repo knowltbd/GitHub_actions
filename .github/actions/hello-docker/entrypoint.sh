bc5cb100ba1137e5996080dc79913c24c2bb81e1
Brian Knowlton <brian.knowlton@relx.com>
2023-04-19 16:29:35 -0400

    add Docker action

---
+++ .github/actions/hello-docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -l
+
+# output strings that the runner will identify for particular logging levels
+echo "::debug:: Debug message"
+echo "::warning:: Warning message"
+echo "::error:: Error message"
+
+# hide a secret value
+echo "::add-mask::$1"
+echo "Hello $1"
+
+# create and output from the process
+time=`date`
+echo "::set-output name=time::$time"
+
+# create a grouped logging
+echo "::group::Some expandable logs"
+echo "some logging"
+echo "some more logging"
+echo "::endgroup::"
+
+# sent an environmental value
+echo "::set-env name=HELLO::hello"
